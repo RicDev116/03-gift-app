4eee0f0a4f1ef62d3868ce82ef4b132d6f406ff4
RicDev116 <ricardo_29leyva@outlook.com>
2024-01-09 16:41:15 -0600

    diseño del gifs components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SharedModule } from './shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, SharedModule]
+    imports: [CommonModule, RouterOutlet, SharedModule,GifsModule]
 })
 export class AppComponent {
   title = 'gift-app';
